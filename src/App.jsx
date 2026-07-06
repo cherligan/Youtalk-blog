e40bdb83580bc8d4dc7191e02e0243946616c193
cherligan <259553831+cherligan@users.noreply.github.com>
2026-07-06 14:40:40 +0300

    feat: add header toggleMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import './App.css'
-
+import { useState } from 'react'
 export default function App() {
-
+const [isOpen, setIsOpen] = useState(false);
+const burgerClick = () =>
+{
+  setIsOpen(!isOpen);
+}
   return (
     <div>
       <header className='header'>
         <img src='./assets/logo.png' className='header__logo' alt='logo'></img>
         <nav className='header__nav'>
-          <a className='header__nav--item'>О нас</a>
-          <a className='header__nav--item'>Тарифы</a>
-          <a className='header__nav--item'>Для бизнеса</a>
-          <a className='header__nav--item'>Психологам</a>
-          <a className='header__nav--item'>Сертификат</a>
-          <a className='header__nav--item'>Материалы <img src='./assets/arD.png' alt='arrow'></img></a>
-          <a className='header__nav--item'>FAQ</a>
+          <a href='#about' className='header__nav--item'>О нас</a>
+          <a href='#tariffs' className='header__nav--item'>Тарифы</a>
+          <a href='#business' className='header__nav--item'>Для бизнеса</a>
+          <a href='#psychologists' className='header__nav--item'>Психологам</a>
+          <a href='#certificate' className='header__nav--item'>Сертификат</a>
+          <a href='#materials' className='header__nav--item'>Материалы <img src='./assets/arD.png' alt='arrow'></img></a>
+          <a href='#faq' className='header__nav--item'>FAQ</a>
         </nav>
         <button className='header__btn'>Подобрать психолога</button>
+        <button className='header__toggle-menu' onClick={burgerClick}><img src='./assets/toggleMenu.png' alt='toggleMenu'></img></button>
+        {isOpen && 
+        (
+          <div className='header__mobile-nav-pannel'>
+              <a href='#about' className='header__nav--item' onClick={() => setIsOpen(false)}>О нас</a>
+              <a href='#tariffs' className='header__nav--item' onClick={() => setIsOpen(false)}>Тарифы</a>
+              <a href='#business' className='header__nav--item' onClick={() => setIsOpen(false)}>Для бизнеса</a>
+              <a href='#psychologists' className='header__nav--item' onClick={() => setIsOpen(false)}>Психологам</a>
+              <a href='#certificate' className='header__nav--item' onClick={() => setIsOpen(false)}>Сертификат</a>
+              <a href='#materials' className='header__nav--item' onClick={() => setIsOpen(false)}>Материалы</a>
+              <a href='#faq' className='header__nav--item' onClick={() => setIsOpen(false)}>FAQ</a>
+          </div>
+        )}
       </header>
       <main className='main'>
         <span className='main__path-to-blog'>Главная <img src='./assets/arR.png' alt='arrow'></img> Блог</span>
